eb89db18bebe6d63d9b2e3a8a793b1f22c65c57e
Elena-Vaganova4 <37454092+Elena-Vaganova4@users.noreply.github.com>
2018-06-10 00:28:15 +0500

    Add files via upload

---
+++ Hotel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public class Hotel
+    {
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public double PhoneNumber { get; set; }
+        public StarsRoom ServiceRoom { get; set; }
+        public TypesRoom Room { get; set; }
+        public List<string> addService { get; set; }
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Service
+    {
+        public StarsRoom ServiceRoom { get; set; }
+    }
+    public enum StarsRoom
+    {
+        AllInclusive,
+        Breakfasts,
+        AllIsOf
+    }
+    public class Room
+    {
+        public TypesRoom Rooms { get; set; }
+    }
+    public enum TypesRoom
+    {
+        Family,
+        Double,
+        Single
+    }
+}
